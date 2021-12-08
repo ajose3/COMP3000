983a30b4a7ba6d2619d4a3e8500d4a9c90dce823
ajose3 <57364060+ajose3@users.noreply.github.com>
2021-12-08 14:33:12 +0000

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -4,3 +4,7 @@ CourseID int IDENTITY(1,1) PRIMARY KEY,
 CourseName VARCHAR(250) NOT NULL,
 CourseDescription TEXT
 );
+
+/*Insert statement*/
+INSERT INTO dbo.Course (CourseName, CourseDescription)
+VALUES ('BSc Computer Science', 'This includes programming languages, software engineering, artificial intelligence, operating systems, cyber security, statistical and mathematical models of computation, machine learning and the theories of computation.');
